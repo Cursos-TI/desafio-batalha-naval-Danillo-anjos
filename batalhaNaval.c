c2724352275c24ab2027e0839b326a4d20533091
Luiz Danillo Morais <144749590+Danillo-anjos@users.noreply.github.com>
2025-05-12 17:07:29 +0000

    Correção de erros de declaração de variáveis: Nível novato completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,13 +4,11 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-#include <stdio.h>
 
 // Define o tamanho fixo do tabuleiro e dos navios
 #define TAM_TABULEIRO 10
@@ -127,5 +125,4 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    return 0;
-}
+
